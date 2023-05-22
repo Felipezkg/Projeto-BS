96bfa1ec0e23e7ab5f57a7079e3ccf2a2c5473d2
Felipezkg <felipezkg@gmail.com>
2023-05-22 14:31:51 -0300

    Commit (22/05) - 4 Telas - Sem Integração com BD.

--- App.js
+++ App.js
@@ -1,20 +1,56 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+//---------------------------------------------------------------------------------------------------------------------------------------
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+//---------------------------------------------------------------------------------------------------------------------------------------
+const Stack = createStackNavigator();
+//---------------------------------------------------------------------------------------------------------------------------------------
+import { HomeScreen } from './pages/HomeScreen';
+import { CadpaisScreen } from './pages/CadpaisScreen';
+import { CadbabaScreen } from './pages/CadbabaScreen';
+import { LoginScreen } from './pages/LoginScreen';
+//---------------------------------------------------------------------------------------------------------------------------------------
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName={'Home'}>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            headerShown: false,
+            gestureEnabled: true,
+          }}
+        />
+
+        <Stack.Screen
+          name='Cadastro de Pais'
+          component={CadpaisScreen}
+          options={{
+            headerShown: true,
+            gestureEnabled: true,
+          }}
+        />
+
+        <Stack.Screen
+          name='Cadastro de Babás'
+          component={CadbabaScreen}
+          options={{
+            headerShown: true,
+            gestureEnabled: true,
+          }}
+        />
+
+        <Stack.Screen
+          name='Login'
+          component={LoginScreen}
+          options={{
+            headerShown: true,
+            gestureEnabled: true,
+          }}
+        />
+
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ pages/LoginScreen.js
@@ -0,0 +1,97 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Pressable, Image, TextInput } from 'react-native';
+
+import secImg from '../images/Secure-login.png';
+
+export function LoginScreen() {
+
+
+    return (
+        <View style={styles.container}>
+            <StatusBar style='dark' />
+            <Image
+                style={styles.secImg}
+                source={secImg}
+            />
+            <View style={styles.infoBox}>
+                <Text style={styles.boxTitle}>CPF</Text>
+                <TextInput
+                    style={styles.textInput}
+                    keyboardType='numeric'
+                    maxLength={11}
+                    placeholder='000.000.000-00'
+                    placeholderTextColor="gray"
+                />
+            </View>
+            <View style={styles.infoBox}>
+                <Text style={styles.boxTitle}>SENHA</Text>
+                <TextInput
+                    style={styles.textInput}
+                    keyboardType='numeric'
+                    maxLength={6}
+                    placeholder='Até 6 Dígitos.'
+                    placeholderTextColor="gray"
+                />
+            </View>
+            <Pressable style={styles.btn_logar}>
+                <Text style={styles.text_btn}>LOGAR</Text>
+            </Pressable>
+            <Text style={styles.lostPw}>Esqueceu sua Senha?</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+
+    secImg: {
+        width: 300,
+        height: 300,
+    },
+
+    infoBox: {
+        margin: 15,
+        alignItems: 'center',
+    },
+
+    boxTitle: {
+        fontSize: 25,
+        fontFamily: 'Comfortaa_400Regular',
+    },
+
+    textInput: {
+        marginTop: 5,
+        width: 200,
+        height: 40,
+        backgroundColor: '#EBEBEB',
+        borderBottomWidth: 1,
+        borderBottomColor: 'gray',
+        borderRadius: 5,
+    },
+
+    btn_logar: {
+        marginTop: 50,
+        width: 185,
+        height: 50,
+        backgroundColor: '#61D6FF',
+        borderRadius: 8,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    text_btn: {
+        color: '#fff',
+        fontWeight: 800,
+    },
+
+    lostPw: {
+        marginTop: 15,
+        color: '#6C6C6C',
+        fontFamily: 'Comfortaa_300Light',
+    },
+})
